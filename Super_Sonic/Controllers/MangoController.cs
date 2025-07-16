7c3e35e6ab7f715b1e9be6a2b6d3ce21e1d275c1
Badr Elbadry <badr.ahmed.2015@gmail.com>
2025-07-17 01:45:51 +0300

    Adding mango controller

---
+++ Super_Sonic/Controllers/MangoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Super_Sonic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MangoController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Mango is ready to be served!");
+        }
+    }
+}
